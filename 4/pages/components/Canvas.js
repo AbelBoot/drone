a4782c0acb3b1f17bd17b182e5c6ac3cc07dae93
Abel Locati <abellocati@Abels-Air.home>
2020-03-19 10:32:57 +0000

    create new folder for back end and moved  git folder as well

---
+++ 4/pages/components/Canvas.js
@@ -0,0 +1,43 @@
+import React from "react"
+import CanvasStatic from "./CanvasStatic"
+import { main } from "../Utilities/Util"
+
+export default class Canvas extends React.Component {
+
+	canvas = React.createRef()
+
+	state = {
+		previous: null
+	}
+	
+	filming = () => {
+		setInterval(() => {
+			return this.getCanvas()
+		}, 16)
+	}
+
+	getCanvas = () => {
+		const canvas = this.canvas.current.canvasStatic.current
+		const ctx = canvas.getContext("2d")
+		const { width, height } = canvas
+		ctx.drawImage(this.props.video, 0, 0, width, height)
+		this.canvas.current.drawingStatic()
+		const blank = ctx.createImageData(width, height)
+		const current = ctx.getImageData(0, 0, width, height)
+		if (this.state.previous === null){this.setState({previous: current})}
+		main(blank.data, this.state.previous.data, current.data)
+		ctx.putImageData(blank, 0, 0 )
+		this.setState({previous: current})
+		
+
+	}
+
+	render(){
+		return (
+			<>
+			<button onClick={this.filming}>Get Canvas</button>
+			<CanvasStatic ref={this.canvas}/>
+			</>
+			)
+	}
+}
